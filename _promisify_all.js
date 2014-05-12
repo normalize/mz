cc01590c71bd7bdfb0001e85fdf0fadd22bd4bce
Jonathan Ong <jonathanrichardong@gmail.com>
2014-05-11 19:59:18 -0700

    fix deprecated message

--- _promisify_all.js
+++ _promisify_all.js
@@ -3,16 +3,22 @@ var promisify = require('./_promisify.js')
 
 module.exports = function (source, exports, methods) {
   methods.forEach(function (name) {
-    // fucking node and util.deprecate()
-    var desc = Object.getOwnPropertyDescriptor(source, name)
-    if (desc.get) return // fuck this shit
-    
+    if (deprecated(source, name)) return
     if (typeof source[name] === 'function')
       exports[name] = promisify(name, source[name])
   })
 
   // proxy the rest
   Object.keys(source).forEach(function (name) {
-    if (!exports[name]) exports[name] = source[name]
+    if (deprecated(source, name)) return
+    if (exports[name]) return
+    exports[name] = source[name]
   })
+}
+
+function deprecated(source, name) {
+  var desc = Object.getOwnPropertyDescriptor(source, name)
+  if (!desc.get) return false
+  if (desc.get.name === 'deprecated') return true
+  return false
 }
