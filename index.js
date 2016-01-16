03207833f2fea1a1af0e8d120d3dea632fb53cb9
Tomás Aparicio <tomas@aparicio.me>
2016-01-16 14:26:02 +0000

    fix: typo with property name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   fs: require('./fs'),
   dns: require('./dns'),
-  zlid: require('./zlib'),
+  zlib: require('./zlib'),
   crypto: require('./crypto'),
   readline: require('./readline'),
   child_process: require('./child_process')
