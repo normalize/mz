7f0465e3c9b60abc4e28319516ed4de17de35a52
Tomas Aparicio <tomas@aparicio.me>
2016-01-16 00:47:00 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  fs: require('./fs'),
+  dns: require('./dns'),
+  zlid: require('./zlib'),
+  crypto: require('./crypto'),
+  readline: require('./readline'),
+  child_process: require('./child_process')
+}
